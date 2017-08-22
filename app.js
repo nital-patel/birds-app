31af0c6394ff943038f5a6627f7bb77fd8be80fc
Nital Patel <patelnital36@gmail.com>
2017-08-21 22:51:50 -0400

    First commit

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
+const bodyParser = require('body-parser')
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', function (req, res) {
+    res.render('index');
+})
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(require('./routes/birds.js'));
+
 
 app.listen(port);
 // to prove it worked, print to the terminal
